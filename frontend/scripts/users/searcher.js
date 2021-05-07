daa7384a0b2e8f68f0990d6050cfc9cfce043ea0
dmenendezh <diego.andres.menendez@gmail.com>
2021-05-07 15:28:23 -0300

    Sync form create new user

--- frontend/scripts/users/searcher.js
+++ frontend/scripts/users/searcher.js
@@ -26,82 +26,36 @@ const getSearch = async () => {
     if (response.status === 200) {
         console.log("resultado");
         console.log(data);      
-        console.log(data.quantity);       
+        console.log(data.quantity);
+        $dataTable.innerHTML = '';  
+        
+        if(data.quantity == 0){
+            const usrContainer = document.createElement('div');
+            usrContainer.innerHTML = `            
+            No existen coincidencias con el nombre ingresado`;
+            $dataTable.appendChild(usrContainer);
+        }
+
+        for (let i = 0; i < data.quantity; i++) {
+            console.log(data.usr[i].usr_login);
+            let profile = "";
+            if(data.usr[i].usr_admin_flag == 1){
+                profile = "Administrador";
+            }else{
+                profile = "Básico";
+            }
+			const usrContainer = document.createElement('tbody');
+			usrContainer.innerHTML = `            
+            <tr>
+                <td><input type="checkbox"></td>
+                <td>${data.usr[i].usr_login}</td>
+                <td>${data.usr[i].usr_name}</td>
+                <td>${data.usr[i].usr_surname}</td>
+                <td>${data.usr[i].usr_email}</td>
+                <td>${profile}</td>                
+            </tr>
+			`;
+			$dataTable.appendChild(usrContainer);
+        }   
     }
-
-    //const matchedArray = findMatches(searchValue, data);
-    //console.log(matchedArray);
-
 };
-/*
-function search(e) {
-    console.log("escribiendo....")
-
-    const nombre_txt = document.getElementById('inputPassword');
-    const name = {
-        nombre: nombre_txt.value   
-    }
-    const options = {
-        method: 'GET',
-        body: JSON.stringify(name),
-        headers: {    
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': '*'}
-    }
-    const response = await fetch('http://localhost:3000/users/searchUser', options)
-    console.log(response);
-
-    const cities = [];
-
-    const data = await response.json();
-    if (response.status === 200) {
-        console.log("resultado");
-        console.log(data);
-        console.log(data.quantity);
-        cities.push(data);
-    }
-
-    const matchedArray = findMatches(e.target.value, cities);
-    console.log(matchedArray);
-}
-*/
-
-//const searchUser = callSearchUser();
-/*
-function callSearchUser() {
-    const nombre_txt = document.getElementById('inputPassword');
-    const name = {
-        nombre: nombre_txt.value   
-    }
-    const options = {
-        method: 'POST',
-        body: JSON.stringify(name),
-        headers: {    
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': '*'}
-    }
-
-    const response = await fetch('http://localhost:3000/users/searchUser', options);
-    const data = await response.json()
-    if (response.status === 200) {
-        console.log("resultado");
-        console.log(data);
-        console.log(data.quantity);
-    }
-}*/
-
-
-function findMatches(wordToMatch, cities) {
-    console.log("place.quantity:"+ cities.quantity);
-    let persons = [];
-    const regex = new RegExp(wordToMatch, 'gi');
-    for (let index = 0; index < cities.quantity; index++) {
-        console.log(cities.usr[index].usr_name);
-        persons.push(cities.usr[index].usr_name);        
-    }
-    return persons.match(regex);
-}
